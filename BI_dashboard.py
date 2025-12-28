ecae9ff145376a1a60185dae6e858ca9d311d2a5
Rishaa SureshKumar <rishaa.ssk@gmail.com>
2025-12-28 14:30:02 +0100

    Update BI_dashboard.py

--- BI_dashboard.py
+++ BI_dashboard.py
@@ -168,7 +168,7 @@ if page == "👤 Profile & Contact":
             st.download_button(
                 "⬇️ Download my CV (PDF)",
                 pdf_path.read_bytes(),
-                file_name="CV_Apprenticeship_2025_Rishaa",
+                file_name="CV_Apprenticeship_2025_Rishaa.pdf",
                 mime="application/pdf",
                 use_container_width=True
             ) 
